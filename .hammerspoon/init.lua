c1f79ea5748d5952302f39c782313ba6f70a4c13
Oliver <olambo@gmail.com>
2024-05-25 12:29:55 +1000

    dont use timer on control key

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -4,6 +4,5 @@ require("shortcuts")
 -- window handling
 require("window")
 
--- Keyboard Settings -> Modifier Keys -> Change CAPS_LOCK to control
--- Escape when no other key chosen and within short time
+-- control key - Escape when no other key chosen, otherwise control key
 hs.loadSpoon("ControlEscape"):start()
